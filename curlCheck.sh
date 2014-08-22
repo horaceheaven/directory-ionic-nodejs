55bba4170770c101f6bc71233eb4bf97ef8cb7e3
sigismond-medullan <smclaughlin@medullan.com>
2014-08-22 14:41:02 -0500

    Modify curl check

--- curlCheck.sh
+++ curlCheck.sh
@@ -1,8 +1,10 @@
 CODE=
 while true; do
-  CODE=$(curl -sL -w "%{http_code}" "http://localhost:5000/" -o /dev/null)
+  CODE=$(curl -sL -w "%{http_code}" "http://127.0.0.1:5000/" -o /dev/null)
   if [ $CODE -eq "200" ]
-    then exit 0
+   pybot --variable BROWSER:phantomjs robot/test/simple_test_suite.robot 
+   then exit 0
   fi
   sleep 1
 done
+
