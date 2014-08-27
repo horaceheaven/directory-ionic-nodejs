5bcddf82d135c2b3e95bacc31e0c4eb4559ecf51
sigismond-medullan <smclaughlin@medullan.com>
2014-08-27 12:42:41 -0500

    mod startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,16 @@
 #! /usr/bin/env bash
-forever /src/server.js
+cd /src
+forever start server.js
+CODE=
+while true; do
+  CODE=$(curl -sL -w "%{http_code}" "http://127.0.0.1:5000" -o /dev/null)
+  if [ $CODE -eq 200 ]
+   then
+   pybot --variable BROWSER:phantomjs /src/robot/test/simple_test_suite.robot
+   echo $CODE 
+   exit 0
+  fi
+  echo $CODE
+  sleep 1
+done
+
