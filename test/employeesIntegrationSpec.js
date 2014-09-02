fe3dd4c59f9191900929768c7fc96734202e7e32
sigismond-medullan <smclaughlin@medullan.com>
2014-09-02 10:23:41 -0500

    Modify Integration tests

--- test/employeesIntegrationSpec.js
+++ test/employeesIntegrationSpec.js
@@ -4,13 +4,13 @@ var request = require('supertest')
 var app = require('../server.js').app;
 
 
-describe('GET /employees/1', function(){
+describe('GET /api/employees/1', function(){
   it('respond with json', function(done){
     request(app)
-      .get('/employees/1')
+      .get('/api/employees/1/reports')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200)
-      .expect({"id": 1, "firstName": "Julie", "lastName": "Taylor", "managerId": 0, "managerName": "James King", "reports": 2, "title": "VP of Marketing", "department": "Marketing", "cellPhone": "617-000-0002", "officePhone": "781-000-0002", "email": "jtaylor@fakemail.com", "city": "Boston, MA", "pic": "Julie_Taylor.jpg", "twitterId": "@fakejtaylor", "blog": "http://coenraets.org"},done);
+      .expect({"id":1,"firstName":"Julie","lastName":"Taylor","title":"VP of Marketing","pic":"Julie_Taylor.jpg","reports":[{"id":7,"firstName":"Lisa","lastName":"Wong","title":"Marketing Manager","pic":"Lisa_Wong.jpg"},{"id":8,"firstName":"Gary","lastName":"Donovan","title":"Marketing Manager","pic":"Gary_Donovan.jpg"}]},done);
   })
 })
