19804ff2b3b00a8aca81c639d2eb869d35c0e16a
horaceheaven <hheaven@medullan.com>
2014-09-08 14:12:50 -0400

    Updated dao with logs

--- daos/employeesDAO.js
+++ daos/employeesDAO.js
@@ -5,8 +5,10 @@ var MongoClient = require('mongodb').MongoClient
 
 if(process.env["MONGO_DB_NAME"]){
 	connString = 'mongodb://ec2-54-91-88-251.compute-1.amazonaws.com:27017/' + process.env["MONGO_DB_NAME"];
+  console.log("Using environment variable for connection string: " + connString);
 } else {
 	connString = 'mongodb://ec2-54-91-88-251.compute-1.amazonaws.com:27017/test_db';
+  console.log("Fallback connection string: " + connString);
 }
 
 var EmployeeDAO = (function(client, connString, q, collectionName){
