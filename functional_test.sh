5c58c10231bcaf36af16971b2675a49c201bd6e5
horaceheaven <hheaven@medullan.com>
2014-09-16 15:08:10 -0400

    Update functional test script

--- functional_test.sh
+++ functional_test.sh
@@ -1,13 +1,16 @@
 echo "Waiting for application server on $1..."
-echo "REMOTE_URL: $2"
-echo "DESIRED_CAPABILITIES: $3"
+
+echo "REMOTE_URL: $1"
+echo "DESIRED_CAPABILITIES: $2"
 
 CODE=
 while true; do
   CODE=$(curl -sL -w "%{http_code}" "$1" -o /dev/null)
   if [ $CODE -eq "200" ]
     then
+      echo "Start robot tests"
       pybot -v $1 -v $2 $3
+      echo "Finish robot tests"
     exit 0
   fi
   sleep 1
