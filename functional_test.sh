8c1cb7c2773b6ed70b889b8c30f9df7e99189511
horaceheaven <hheaven@medullan.com>
2014-09-18 15:13:21 -0400

    Test Commit

--- functional_test.sh
+++ functional_test.sh
@@ -7,11 +7,11 @@ CODE=
 while true; do
   CODE=$(curl -sL -w "%{http_code}" "$1" -o /dev/null)
   if [ $CODE -eq "200" ]
-    then 
+    then
       echo "Start robot tests"
       pybot -v "$2" -v "$3" "$4"
       echo "Finish robot tests"
     exit 0
-  fi
+  fi 
   sleep 1
 done
