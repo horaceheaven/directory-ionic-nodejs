9a3dd8f740b7eac1a96b5293afce663456a33d84
horaceheaven <hheaven@medullan.com>
2014-10-03 16:20:07 -0400

    Remove wild card from server.js

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.all('*', function(req, res, next) {
     next();
 });
 
-app.get('/*/employees', employees.findAll);
-app.get('/*/employees/:id', employees.findById);
-app.get('/*/employees/:id/reports', employees.findReports);
+app.get('/employees', employees.findAll);
+app.get('/employees/:id', employees.findById);
+app.get('/employees/:id/reports', employees.findReports);
 
 app.set('port', process.env.PORT || 5000);
 
