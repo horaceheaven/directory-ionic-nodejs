5a61bcdd59d8695c5c9c22aee2cf770dccad5c46
sigismond-medullan <smclaughlin@medullan.com>
2014-10-06 13:34:15 -0500

    prefix endpoints with branch name

--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,6 @@
 angular.module('directory.services', ['ngResource'])
 
-    .factory('Employees', function ($resource) {
-        return $resource('/employees/:employeeId/:data');
+    .factory('Employees', function ($resource, $location) {
+        var branch = $location.path().split("/")[1];
+        return $resource( '/' + branch + '/employees/:employeeId/:data');
     });
