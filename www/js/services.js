74df4cd1b4c49cecc83604f3adcc9cb0ea06bf86
horaceheaven <hheaven@medullan.com>
2014-10-06 15:39:24 -0400

    Update split index

--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,5 @@
 angular.module('directory.services', ['ngResource'])
     .factory('Employees', function ($resource, $location) {
-        var branch = $location.path().split("/")[1];
+        var branch = $location.absUrl().split("/")[2];
         return $resource( '/' + branch + '/employees/:employeeId/:data');
     });
