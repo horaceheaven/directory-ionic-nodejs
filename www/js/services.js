829564b0b65d172c98f1d55844a420cfc28347b8
horaceheaven <hheaven@medullan.com>
2014-10-06 13:49:04 -0400

    Updated service

--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,5 @@
 angular.module('directory.services', ['ngResource'])
 
     .factory('Employees', function ($resource) {
-        return $resource('/:qa/employees/:employeeId/:data');
+        return $resource('/latest/employees/:employeeId/:data');
     });
