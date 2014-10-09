9f5d18db8061d02291e5869c286e669b1236617e
sigismond-medullan <smclaughlin@medullan.com>
2014-10-09 15:20:39 -0500

    Remove console.log

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ var express = require('express'),
 var serveStatic = require('serve-static')
 app.use(express.static(path.join(__dirname, 'www')));
 app.use('/' + process.env['NODE_ENV'], serveStatic('www'));
-console.log("using: " + process.env['NODE_ENV']);
-
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
