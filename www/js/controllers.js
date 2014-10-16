86aed39f0cd3e1c748360908425253e7fcc0e957
sigismond-medullan <smclaughlin@medullan.com>
2014-10-16 13:25:51 -0500

    Fix error with controllers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,12 +13,16 @@ angular.module('directory.controllers', [])
             $scope.employees = Employees.query({name: $scope.searchKey});
         }
 
-        $scope.employees = Employees.query();
+        $scope.employees = Employees.query(function(data) {
+            // success handler
+        }, function(error) {
+            console.log(error);
+        });
 
         $log.info('Employee list requested');
     })
 
-    .controller('EmployeeDetailCtrl', function($scope, $stateParams, Employees, $location) {
+    .controller('EmployeeDetailCtrl', function($scope, $stateParams, Employees, $location, $log) {
         var branch;
         console.log('details');
         branch = $location.absUrl().split("/")[3];
@@ -27,7 +31,7 @@ angular.module('directory.controllers', [])
         $log.info('Employee ' + $stateParams.employeeId +' details requested');
     })
 
-    .controller('EmployeeReportsCtrl', function ($scope, $stateParams, Employees) {
+    .controller('EmployeeReportsCtrl', function ($scope, $stateParams, Employees, $log) {
         console.log('reports');
         $scope.employee = Employees.get({employeeId: $stateParams.employeeId, data: 'reports'});
         $log.info('Employee ' + $stateParams.employeeId +' report generated');
